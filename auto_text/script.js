7d207f8a2872e07605a46af20791d2b78043e65b
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-06-05 09:41:36 +0300

    "2.way added"

--- auto_text/script.js
+++ auto_text/script.js
@@ -1,13 +1,44 @@
 const dataCont = document.querySelectorAll('.auto_text');
+//console.log(dataCont);
+const texts = Array.from(dataCont).map((text) => text.getAttribute('data'));
+//console.log(texts);
 
-var texts = [];
-var reverse = false;
+var reverse = false; //write, does not delete
 var idx = 0;
 var txtNum = 0;
 var speed = 100;
 var waitAtTheEnd = 2;
 
-dataCont.forEach((text) => {
+const write = () => {
+    const currentText = texts[txtNum];
+
+    if (!reverse) {
+        if (idx < currentText.length + waitAtTheEnd) {
+            dataCont[0].innerText = currentText.slice(0, idx);
+            idx++;
+        } else {
+            reverse = true;
+            idx--;
+        }
+    } else {
+        if (idx > 0) {
+            dataCont[0].innerText = currentText.slice(0, idx);
+            idx--;
+        } else {
+            reverse = false;
+            txtNum = (txtNum + 1) % texts.length;
+        }
+    }
+};
+
+setInterval(write, speed);
+
+
+
+
+
+
+/* dataCont.forEach((text) => {
     let data = text.getAttribute('data');
     texts.push(data);
 });
@@ -25,4 +56,4 @@ var write = setInterval(() => {
             txtNum == texts.length - 1 ? (txtNum = 0) : txtNum++;
         }
     }
-}, speed);
+}, speed); */
